991863c87ab352e53c6068010aeb6d5f73c90ba1
matthewwen <mattwen2018@gmail.com>
2020-01-27 13:01:45 -0600

    Restart entire project

---
+++ app/src/main/java/com/matthewwen/todoplanner/ui/home/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.matthewwen.todoplanner.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.matthewwen.todoplanner.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private RecyclerView recyclerView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        recyclerView = root.findViewById(R.id.recycle_view_home);
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
